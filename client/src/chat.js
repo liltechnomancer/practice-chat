05eff10c682a9a207b8d9e21c6cd161b5bbcbeaa
Levi  Robertson <levi@lvrbrtsn.com>
2018-01-08 22:58:51 -0700

    begining to add styled components.

--- client/src/chat.js
+++ client/src/chat.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react'
 import { map } from 'ramda'
+import styled from 'react-emotion'
+
+const Layout = styled('div')`
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 90%;
+  grid-template-columns: 90% 10%;
+`
+
+const MessageBox = styled('div')`
+  overflow: scroll;
+  grid-column: 1 / span 2;
+`
+
+const Form = styled('form')`
+  grid-column 1 / span 2;
+  height: 100%;
+  width: 100%;
+`
 
 class Chat extends Component {
   constructor() {
@@ -21,17 +41,26 @@ class Chat extends Component {
       this.props.messages
     )
     return (
-      <div>
-        <ul>{allMessages}</ul>
-        <input
-          type="text"
-          value={this.state.message}
-          onChange={e => this.handleMsgChange(e.target.value)}
-        />
-        <button onClick={_ => this.props.handleSubmit(this.state.message)}>
-          Send
-        </button>
-      </div>
+      <Layout>
+        <MessageBox>
+          <ul>{allMessages}</ul>
+        </MessageBox>
+        <Form>
+          <input
+            type="text"
+            value={this.state.message}
+            onChange={e => this.handleMsgChange(e.target.value)}
+          />
+          <button
+            onClick={e => {
+              e.preventDefault()
+              this.props.handleSubmit(this.state.message)
+            }}
+          >
+            Send
+          </button>
+        </Form>
+      </Layout>
     )
   }
 }
