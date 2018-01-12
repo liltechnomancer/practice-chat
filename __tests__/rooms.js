671f8a8e7147047cbf8ade8fdcddbd1bc0022536
Levi Robertson <robertson.l.levi@gmail.com>
2018-01-12 02:12:01 -0700

    making immutable rooms

--- __tests__/rooms.js
+++ __tests__/rooms.js
@@ -1,8 +1,12 @@
 import Rooms from '../src/rooms'
+import { Map } from 'immutable'
 
 test('Allows rooms to be created and joined', () => {
   const rooms = Rooms()
-  const newRoom = { id: 1, sockets: new Map() }
-  expect(rooms.join(1, 'test')).toEqual(newRoom)
+  const ws = { id: 1 }
+  const newRoom = { id: 1, sockets: Map().set(ws.id, ws) }
+  const emptyRoom = { id: 1, sockets: Map() }
+  expect(rooms.join(1, ws).rooms).toEqual(newRoom)
+  expect(rooms.leave(1, ws).rooms).toEqual(emptyRoom)
   rooms.join(1, 'test')
 })
--- src/rooms.js
+++ src/rooms.js
@@ -1,34 +1,49 @@
-const Rooms = () => {
-  const rooms = new Map()
+import { Map } from 'immutable'
+import { lens, prop, assoc, set } from 'ramda'
+
+const socketLens = lens(prop('sockets'), assoc('sockets'))
+
+const Rooms = (rooms = Map()) => {
 
   const makeRoom = id => {
     const room = {
       id,
-      sockets: new Map()
+      sockets: Map()
     }
-    rooms.set(id, room)
     return room
   }
 
   const getRoom = id => {
     const room = rooms.get(id)
     if (!room) return makeRoom(id)
-    console.log('yay')
     return room
   }
 
+  const addWsToRoom = (ws, room) => 
+    room.sockets.set(ws.id, ws)
+
+  const removeWsFromRoom = (ws, room) => 
+    room.sockets.delete(ws.id, ws)
+
+
+  //TODO: Rooms should be a monad & join and leave should be passed into flatMap/chain.
   const join = (id, ws) => {
     const room = getRoom(id)
-    return room
+    return Rooms(set(socketLens, addWsToRoom(ws, room), room))
   }
 
-  const leave = (opts, ws) => {}
+  const leave = (id, ws) => {
+    const room = getRoom(id)
+    return Rooms(set(socketLens, removeWsFromRoom(ws, room), room))
+  }
+
   const message = (opts, ws) => {}
 
   return {
     join,
     leave,
-    message
+    message,
+    rooms
   }
 }
 
