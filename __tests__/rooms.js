9e26c2f8dc7b51d77f82e00e160770d5bfa4bdc3
Levi Robertson <robertson.l.levi@gmail.com>
2018-01-12 02:42:38 -0700

    making Rooms monadic. seperated functions as exports

--- __tests__/rooms.js
+++ __tests__/rooms.js
@@ -1,12 +1,13 @@
-import Rooms from '../src/rooms'
+import { Rooms, join, leave } from '../src/rooms'
 import { Map } from 'immutable'
 
-test('Allows rooms to be created and joined', () => {
+test('Allows rooms to be created and joined, as well as left.', () => {
   const rooms = Rooms()
   const ws = { id: 1 }
   const newRoom = { id: 1, sockets: Map().set(ws.id, ws) }
   const emptyRoom = { id: 1, sockets: Map() }
-  expect(rooms.join(1, ws).rooms).toEqual(newRoom)
-  expect(rooms.leave(1, ws).rooms).toEqual(emptyRoom)
-  rooms.join(1, 'test')
+  const joinOne = join(1)
+  const leaveOne = leave(1)
+  expect(rooms.map(joinOne(ws)).id()).toEqual(newRoom)
+  expect(rooms.map(leaveOne(ws)).id()).toEqual(emptyRoom)
 })
--- src/rooms.js
+++ src/rooms.js
@@ -5,46 +5,56 @@ const socketLens = lens(prop('sockets'), assoc('sockets'))
 
 const Rooms = (rooms = Map()) => {
 
-  const makeRoom = id => {
-    const room = {
-      id,
-      sockets: Map()
-    }
-    return room
-  }
 
-  const getRoom = id => {
-    const room = rooms.get(id)
-    if (!room) return makeRoom(id)
-    return room
-  }
+  const map = (fn) => Rooms(fn(rooms))
+  const chain = fn => fn(rooms)
+  const id = _ => rooms
 
-  const addWsToRoom = (ws, room) => 
-    room.sockets.set(ws.id, ws)
-
-  const removeWsFromRoom = (ws, room) => 
-    room.sockets.delete(ws.id, ws)
-
-
-  //TODO: Rooms should be a monad & join and leave should be passed into flatMap/chain.
-  const join = (id, ws) => {
-    const room = getRoom(id)
-    return Rooms(set(socketLens, addWsToRoom(ws, room), room))
-  }
-
-  const leave = (id, ws) => {
-    const room = getRoom(id)
-    return Rooms(set(socketLens, removeWsFromRoom(ws, room), room))
-  }
-
+  //TODO: Making Rooms monadic, message should be seperate.
   const message = (opts, ws) => {}
 
   return {
-    join,
-    leave,
-    message,
-    rooms
+    chain,
+    map,
+    id 
   }
 }
 
+const makeRoom = id => {
+  const room = {
+    id,
+    sockets: Map()
+  }
+  return room
+}
+
+const addWsToRoom = (ws, room) => 
+  room.sockets.set(ws.id, ws)
+
+const removeWsFromRoom = (ws, room) => 
+  room.sockets.delete(ws.id, ws)
+
+const getRoom = (id, rooms) => {
+  const room = rooms.get(id)
+  if (!room) return makeRoom(id)
+  return room
+}
+
+const join = id => ws => rooms => {
+  const room = getRoom(id, rooms)
+  return set(socketLens, addWsToRoom(ws, room), room)
+}
+
+const leave = id => ws => rooms => {
+  const room = getRoom(id, rooms)
+  return set(socketLens, removeWsFromRoom(ws, room), room)
+}
+
 export default Rooms
+
+export {
+  Rooms,
+  join,
+  leave
+}
+
